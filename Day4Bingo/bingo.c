ef08ab7653b32ae6dc07ace9be23b86fdd5f17c1
Nick <nicholas.g.wright86@gmail.com>
2021-12-08 21:17:33 +0800

    I can now read any bingo file

--- Day4Bingo/bingo.c
+++ Day4Bingo/bingo.c
@@ -17,9 +17,10 @@ void fileSize( FILE* bingoFilePtr, int* drawsPtr, int* cardNumPtr )
 /*    int nRead = 0;*/
     int done = FALSE;
     int input1;
-    int count = 1; /*Starts at one to factor first strtok read*/
     char *readLine = (char*)malloc( sizeof(char) * 1064 );
     char *temp = (char*)malloc( sizeof(char)* 52 );
+    char *endCheck;
+    (*drawsPtr) = 1; /*Starts at one to factor first strtok read*/
 
     fgets( readLine, 1000, bingoFilePtr );
     input1 = atoi( strtok( readLine, "," ) );
@@ -29,7 +30,7 @@ void fileSize( FILE* bingoFilePtr, int* drawsPtr, int* cardNumPtr )
         temp = strtok( NULL, "," );
         if( temp != NULL )
         {
-            ++count;
+            ++(*drawsPtr);
             input1 = atoi( temp );
             printf( "Input reads %d\n", input1 );
         }
@@ -38,7 +39,22 @@ void fileSize( FILE* bingoFilePtr, int* drawsPtr, int* cardNumPtr )
             done = TRUE;
         }
     }
-    printf( "The final count is %d\n", count );
+    printf( "The final count is %d\n", *drawsPtr );
+    
+    done = FALSE;
+    while( !done )
+    {
+        endCheck = fgets( readLine, 1000, bingoFilePtr );
+        if( !strcmp( readLine, "\n" ) )
+        {
+            (*cardNumPtr)++;
+            printf("Card number is %d\n", *cardNumPtr );
+        }
+        else if( endCheck == NULL )
+        {
+            done = TRUE;
+        }
+    }
 }
 
 
