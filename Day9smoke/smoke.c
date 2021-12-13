2848b277b6d640d61f05fddbee04e0bedbeefb6d
Clusterpuck <nicholas.g.wright86@gmail.com>
2021-12-13 21:37:49 +0800

    Attempting part 2 recursion

--- Day9smoke/smoke.c
+++ Day9smoke/smoke.c
@@ -24,14 +24,14 @@ void fillMap( FILE* smokeFilePtr, int **arrayMap, int rows, int cols )
                 ( i == 0 ) || ( i == ( rows+1 ) ) )
             {/*Inserts a border value greater than all other array values*/
                 arrayMap[i][j] = BORDER_VAL;
-            }   
+            }
             else
             {
                 arrayMap[i][j] = fgetc( smokeFilePtr ) - 48;
             }
         }
         if( ( i != 0 ) && ( i != ( rows+1 ) ) )
-        { 
+        {
             fgetc( smokeFilePtr );
         }
         /*consumes end of line character*/
@@ -39,11 +39,34 @@ void fillMap( FILE* smokeFilePtr, int **arrayMap, int rows, int cols )
     printTwoDIntArray( arrayMap, rows+2, cols+2 );
 }
 
+void sizeBasins( int** basinData, int count,
+                 int** arrayMap, int rows, int cols )
+{
+    int i;
+    for( i = 0; i < count; i++ )
+    {
+        checkAround( arrayMap, rows, cols,
+                     basinData[i][0], basinData[i][1], &( basinData[i][2] ),
+                     arrayMap[basinData[i][0]][basinData[i][1]] );
+    }
+
+    printTwoDIntArray( basinData, count, 3 );
+}
+
 
 int sumRisk( int** arrayMap, int rows, int cols )
 {
     int sum = 0;
     int i, j;
+    int count = 0;
+    int **basinData = (int**)malloc( sizeof(int*) * 1000 );
+    for( i=0; i < 1000; i++ )
+    {
+        basinData[i] = (int*)malloc( sizeof( int ) * 3 );
+        /*To contain i and j values of start of basin in 0 and 1
+ *        and then 2 will contain the size of the basin*/
+    }
+
         /*starts at 1 to account for inserted border*/
     for( i=1; i < ( rows+1) ; i++ )
     {
@@ -54,12 +77,54 @@ int sumRisk( int** arrayMap, int rows, int cols )
             {
                 sum += ( arrayMap[i][j]+1 );
                 printf("Low point at %d,%d, value %d\n", i,j, arrayMap[i][j] );
+
+                basinData[count][0] = i;
+                basinData[count][1] = j;
+                basinData[count][2] = 1;
+                ++count;
             }
         }
     }
+
+    sizeBasins( basinData, count, arrayMap, rows, cols );
     return sum;
 }
 
+void checkAround( int** arrayMap, int rows, int cols,
+                  int i, int j, int* count, int checkVal )
+{
+    int newCheck;
+    checkVal = POS;
+    POS = MARK;
+    if( ( i > 0 ) && ( UP >= checkVal ) && ( UP < 9 ) )
+    {
+        (*count)++;
+        --i;
+        newCheck = POS;
+        checkAround( arrayMap, rows, cols, i, j, count, newCheck );
+    }
+    if( ( DOWN >= checkVal ) &&  ( DOWN < 9 ) )
+    {
+        ++(*count);
+        ++i;
+        newCheck = POS;
+        checkAround( arrayMap, rows, cols, i, j, count, newCheck );
+    }
+    if( ( j > 0 ) && ( LEFT >= checkVal ) && ( LEFT < 9 ) )
+    {
+        (*count)++;
+        --j;
+        newCheck = POS;
+        checkAround( arrayMap, rows, cols, i, j, count, newCheck );
+    }
+    if( ( RIGHT >= checkVal ) && ( RIGHT < 9 ) )
+    {
+        (*count)++;
+        ++j;
+        newCheck = POS;
+        checkAround( arrayMap, rows, cols, i, j, count, newCheck );
+    }
+}
 
 int readFile( FILE* smokeFilePtr )
 {
--- Day9smoke/smoke.h
+++ Day9smoke/smoke.h
@@ -2,15 +2,21 @@
 #define SMOKE_H
 
 #define BORDER_VAL 10
+#define MARK -1
 
 #define UP arrayMap[i-1][j]
 #define DOWN arrayMap[i+1][j]
 #define LEFT arrayMap[i][j-1]
 #define RIGHT arrayMap[i][j+1]
+#define POS arrayMap[i][j]
 
 
 void fillMap( FILE* smokeFilePtr, int **arrayMap, int rows, int cols );
 int sumRisk( int **arrayMap, int rows, int cols );
+void checkAround( int **arrayMap, int rows, int cols, int i, int j,
+                 int *count, int checkVal );
+void sizeBasins( int **basinData, int count,
+                 int **arrayMap, int rows, int cols );
 int readFile( FILE* smokeFilePtr );
 
 #endif
